2b1a4c77378edae62a5c62f3e11e38bdb841dae4
Touch <Kittitat.main@gmail.com>
2022-12-21 22:47:24 +0700

    Optimized

--- 70/2.py
+++ 70/2.py
@@ -1,21 +1,16 @@
 userInput = input().lower() + " "
-
 vowels = ""
 count = 0
-isVowels = False
 
 for i in userInput:
     if i in "aeiou":
         vowels += i
-        isVowels = True
-    else :
-        if isVowels:
-            print(vowels, end = " ")
-            count += 1
-            isVowels = False
-            vowels = ""
+    elif vowels:
+        print(vowels, end=" ")
+        count += 1
+        vowels = ""
 
 if count == 0:
     print("0")
-else :
+else:
     print("=", count)
