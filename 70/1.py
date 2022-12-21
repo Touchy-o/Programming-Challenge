9ebf1c3e442cf5f41fa1765af08d22ad54d2b5cf
Touch <Kittitat.main@gmail.com>
2022-12-21 22:50:29 +0700

    Optimized

--- 70/1.py
+++ 70/1.py
@@ -1,20 +1,20 @@
 userInput = input()
 
 numText = {
-    0 : "ZERO",
-    1 : "ONE",
-    2 : "TWO",
-    3 : "THREE",
-    4 : "FOUR",
-    5 : "FIVE",
-    6 : "SIX",
-    7 : "SEVEN",
-    8 : "EIGHT",
-    9 : "NINE"
+    "0" : "ZERO",
+    "1" : "ONE",
+    "2" : "TWO",
+    "3" : "THREE",
+    "4" : "FOUR",
+    "5" : "FIVE",
+    "6" : "SIX",
+    "7" : "SEVEN",
+    "8" : "EIGHT",
+    "9" : "NINE"
 }
 
 if userInput.isdigit():
     for i in userInput:
-        print(numText[int(i)], end = " ")
+        print(numText[i], end = " ")
 else:
     print("Invalid input : Please input only the numbers")
